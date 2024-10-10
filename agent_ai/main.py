6acb2d3594b97e6827a75a7eb2faa802f032820d
Paul Mathew <paul@kali.paul.com>
2024-10-10 09:35:44 +0530

    fine tune jsonl

--- agent_ai/main.py
+++ agent_ai/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Request, Depends
 from fastapi.responses import Response, JSONResponse
 from agent_ai.gpt import generate_response
-# from agent_ai.fine_tune import check_fine_tune_status
+from agent_ai.fine_tune import check_fine_tune_status
 from twilio.twiml.messaging_response import MessagingResponse
 from agent_ai.schemas import schemas
 from sqlalchemy.orm import Session
 from agent_ai.models.database import get_db
+import asyncio
+
 # Initialize FastAPI app
 app = FastAPI()
 
@@ -26,14 +28,13 @@ async def whatsapp_response(request: Request, db: Session = Depends(get_db)):
     # Return the response as XML
     return Response(content=str(content), media_type="application/xml")
 
-"""
-
 @app.get("/fine_tune")
 async def setup_fine_tune() -> str:
-    if check_fine_tune_status():
+    if await asyncio.to_thread(check_fine_tune_status):
         return "Fine tune setup"
     return "Max retries exceeded. Unable to retrieve the status."
-"""
+
+
 
 # For local testing without Twilio (optional endpoint)
 @app.post("/test")
